b7dbae27249df77b6531d58df3fb85e4b6de001b
mzorkany <63791327+mzorkany@users.noreply.github.com>
2026-05-13 16:01:44 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,6 @@ module.exports = {
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
-    'plugin:import/errors',
     'plugin:import/warnings',
     'plugin:prettier/recommended' // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
@@ -28,7 +27,6 @@ module.exports = {
     '@typescript-eslint/restrict-plus-operands': ['off'], // Allows plus operator to be used between different types
     '@typescript-eslint/require-await': ['off'], // Allows async without await
     'import/no-unresolved': ['off'], // Disable non working rule
-    'import/named': ['off'],
     'import/order': ['error', { 'newlines-between': 'always' }], // Orders imports by ['builtin', 'external', 'parent', 'sibling', 'index']
 
     // Server specific
